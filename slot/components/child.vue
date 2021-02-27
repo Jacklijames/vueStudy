ead757367180741bd6f8a8735ba90475d18e97db
liiiiiibpm <1653433835@qq.com>
2021-02-28 00:44:49 +0800

    slot

---
+++ slot/components/child.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        子组件
+        <!-- :data 是自定义属性？？？？ -->
+        <slot  :data='data' />
+    </div>
+</template>
+<script>
+export default {
+    name:"childTemplate",
+    // props:['user'],
+    data() {
+        return {
+            data:['MonDay','TuesDay','Wednesday','Thursday','Friday','Saturday','Sunday']
+        }
+    },
+}
+</script>
---
+++ slot/components/parent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <child-template>
+            <template slot-scope="slotProps">
+                <ul>
+                    <li v-for="(item, index) in slotProps.data" :key="index">
+                        {{item}}
+                    </li>
+                </ul>
+            </template>
+        </child-template>
+    </div>
+</template>
+<script>
+
+// 导入子组件
+import {childTemplate} from './child'
+
+export default {
+  components: { childTemplate },
+    name:"parentTempt"
+    
+}
+</script>
